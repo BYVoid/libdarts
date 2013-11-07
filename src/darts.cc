bfbffc0821d3c3305f1ca2bdd2ed0327a8d4e7ba
Carbo Kuo <byvoid1@gmail.com>
2013-11-08 06:41:03 +0800

    Add a wrapper struct to preserve exception

--- src/darts.cc
+++ src/darts.cc
@@ -3,51 +3,65 @@
 
 using namespace Darts;
 
+struct DoubleArrayTrie {
+  DoubleArray* da;
+  const char* err;
+  
+  DoubleArrayTrie() : da(new DoubleArray()), err(NULL) {}
+  ~DoubleArrayTrie() {
+    delete da;
+  }
+};
+
 darts_t darts_new(void) {
-  DoubleArray* inst = new DoubleArray();
-  return inst;
+  return new DoubleArrayTrie();
 }
 
 void darts_delete(darts_t darts) {
-  DoubleArray* inst = (DoubleArray*) darts;
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
   delete inst;
+}
+
+const char* darts_error(const darts_t darts) {
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->err;
 }
 
 void darts_set_array(darts_t darts,
                      const void *ptr,
                      size_t size) {
-  DoubleArray* inst = (DoubleArray*) darts;
-  inst->set_array(ptr, size);
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
+  inst->da->set_array(ptr, size);
 }
 
 const void* darts_array(const darts_t darts) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->array();
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->array();
 }
 
 void darts_clear(darts_t darts) {
-  DoubleArray* inst = (DoubleArray*) darts;
-  inst->clear();
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
+  inst->da->clear();
 }
 
 size_t darts_unit_size(const darts_t darts) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->unit_size();
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->unit_size();
 }
 
 size_t darts_size(const darts_t darts) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->size();
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->size();
 }
 
 size_t darts_total_size(const darts_t darts) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->total_size();
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->total_size();
 }
 
 size_t darts_nonzero_size(const darts_t darts) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->nonzero_size();
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->nonzero_size();
 }
 
 int darts_build(darts_t darts,
@@ -56,8 +70,13 @@ int darts_build(darts_t darts,
                 const size_t* lengths,
                 const darts_value_type* values,
                 int (*progress_func)(size_t, size_t)) {
-  DoubleArray* inst = (DoubleArray*) darts;
-  return inst->build(num_keys, keys, lengths, values, progress_func);
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
+  try {
+    return inst->da->build(num_keys, keys, lengths, values, progress_func);
+  } catch (Details::Exception e) {
+    inst->err = e.what();
+    return -1;
+  }
 }
 
 int darts_open(darts_t darts,
@@ -65,24 +84,33 @@ int darts_open(darts_t darts,
                const char* mode,
                size_t offset,
                size_t size) {
-  DoubleArray* inst = (DoubleArray*) darts;
-  return inst->open(file_name, mode, offset, size);
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
+  try {
+    return inst->da->open(file_name, mode, offset, size);
+  } catch (Details::Exception e) {
+    inst->err = e.what();
+    return -1;
+  }
 }
 
 int darts_save(const darts_t darts,
                const char* file_name,
                const char* mode,
                size_t offset) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->save(file_name, mode, offset);
+  DoubleArrayTrie* inst = (DoubleArrayTrie*) darts;
+  int retval = inst->da->save(file_name, mode, offset);
+  if (retval != 0) {
+    inst->err = "Error saving file.";
+  }
+  return retval;
 }
 
 darts_value_type darts_exact_match_search(const darts_t darts,
                                           const darts_key_type* key,
                                           size_t length,
                                           size_t node_pos) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->exactMatchSearch<darts_value_type>(key, length, node_pos);
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->exactMatchSearch<darts_value_type>(key, length, node_pos);
 }
 
 size_t darts_common_prefix_search(const darts_t darts,
@@ -91,8 +119,8 @@ size_t darts_common_prefix_search(const darts_t darts,
                                   size_t max_num_results,
                                   size_t length,
                                   size_t node_pos) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->commonPrefixSearch(key,
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->commonPrefixSearch(key,
                                   results,
                                   max_num_results,
                                   length,
@@ -104,6 +132,6 @@ darts_value_type darts_traverse(const darts_t darts,
                                 size_t* node_pos,
                                 size_t* key_pos,
                                 size_t length) {
-  const DoubleArray* inst = (const DoubleArray*) darts;
-  return inst->traverse(key, *node_pos, *key_pos, length);
+  const DoubleArrayTrie* inst = (const DoubleArrayTrie*) darts;
+  return inst->da->traverse(key, *node_pos, *key_pos, length);
 }
